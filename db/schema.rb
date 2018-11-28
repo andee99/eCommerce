e78f8045c48774839c2a0ba931e88bc0fed684b2
andee99 <andeerakut14@gmail.com>
2018-11-28 16:53:54 +0100

    yhh

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 2018_11_27_054945) do
+
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
 
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
